0f2854ddfcbf2e253334ac99db9108a2a15d3b55
ahmadreza <amiraligoodboy892@gmail.com>
2024-01-24 09:22:52 -0800

    initial code

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
